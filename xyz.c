6a8aee95be9417c781e574e51b0a9508733c82e4
Aryansaini001 <aryan945769@gmail.com>
2024-09-27 15:47:39 +0530

    Add files via upload

---
+++ xyz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a,b,temp;
+    printf("enter first number(a):");
+    scanf("%d",&b);
+    printf("entyer second number(b):");
+  scanf("%d",&b);
+   
+   printf("befor swapping:a=%d,b=%d\n",a,b);
+    temp=a;
+    a=b;
+    b=temp;
+
+    printf("after swapping:a=%d,b=%d\n,a,b");
+    return(0);
+
+}
